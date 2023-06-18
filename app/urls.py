8037be185cfe5dd05285cf23b35a4679c4ce5c8a
Ilham Nuryanto <ilhamnyto@gmail.com>
2023-06-18 18:17:07 +0700

    fix urlpatterns

--- app/urls.py
+++ app/urls.py
@@ -14,12 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 from .views import index
+from django.contrib import admin
+from django.conf import settings
+from django.urls import path, include
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('api/v1/', include('api.urls'), name='scrape')
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
