0ab5e6f384fab91c7da033bec2558fbd6d2addc9
Ilham Nuryanto <ilhamnyto@gmail.com>
2023-06-18 18:27:48 +0700

    fix: url

--- manage.py
+++ manage.py
@@ -2,9 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from dotenv import load_dotenv
 
-load_dotenv()
 
 def main():
     """Run administrative tasks."""
@@ -17,7 +15,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(["manage.py", "runserver", os.getenv("PORT")])
+    execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
